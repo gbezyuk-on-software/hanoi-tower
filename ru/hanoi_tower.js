e17b737687d1cf30a171d93ebbda7f9f19387789
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-25 19:30:40 +0300

    two disks: step 3, what about solve_hanoi(2)\?

--- ru/hanoi_tower.js
+++ ru/hanoi_tower.js
@@ -24,7 +24,9 @@ function move (departure_rod, destination_rod) {
 function solve_hanoi (N, departure_rod, destination_rod) {
     if (N == 1) {
         move(departure_rod, destination_rod);
-        // изменилось только лишь то, что мы заменили константы 1 и 3
-        // на параметры departure_rod и destination_rod
+    }
+    if (N == 2) {
+        // а вот здесь стоит подумать:
+        // какие именно перемещения нам необходимы для переноса пирамидки из двух дисков?
     }
 }

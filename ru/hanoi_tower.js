ca2d000121c98dce807547be68573ce96631c13c
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-25 19:22:49 +0300

    fourth attempt to formulate solution for a single disk

--- ru/hanoi_tower.js
+++ ru/hanoi_tower.js
@@ -1,4 +1,8 @@
 function move (departure_rod, destination_rod) {
+    // 1. снять диск со стержня departure_rod
+    // (то есть удалить его из массива hanoi_1[departure_rod])
+    // 2. положить снятый диск на destination_rod
+    // (то есть добавить его к массиву hanoi_1[destination_rod])
 }
 
 function solve_hanoi (N, departure_rod, destination_rod) {

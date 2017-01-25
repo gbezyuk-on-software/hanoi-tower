76d31e793c7c39025bdc951336cbc61413a196fb
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-25 19:31:28 +0300

    two disks: step 4, get_intermediate_rod introduced

--- ru/hanoi_tower.js
+++ ru/hanoi_tower.js
@@ -13,6 +13,21 @@ function is_move_possible (departure_rod, destination_rod) {
     return destination_rod[0] > departure_rod[0];
 }
 
+function get_intermediate_rod (departure_rod, destination_rod) {
+    // исходный массив стержней — просто три цифры
+    all_rods = [0, 1, 2]
+    
+    // отфильтруем departure_rod встроенной функцией
+    rods_without_departure = all_rods.filter(function (rod) { return rod != departure_rod })
+    
+    // отфильтруем destination_rod встроенной функцией
+    only_one_rod_left = rods_without_departure.filter(function (rod) { return rod != destination_rod })
+    
+    // в all_rods должен к этому моменту остаться только один элемент,
+    // его и вернём как номер промежуточного стержня:
+    return only_one_rod_left[0]
+}
+
 function move (departure_rod, destination_rod) {
     // ранее описанные действия по перекладыванию диска будем выполнять только если это разрешено правилами
     if (is_move_possible(departure_rod, destination_rod)) {
@@ -23,10 +38,21 @@ function move (departure_rod, destination_rod) {
 
 function solve_hanoi (N, departure_rod, destination_rod) {
     if (N == 1) {
+        // просто перекладываем один диск
         move(departure_rod, destination_rod);
     }
     if (N == 2) {
-        // а вот здесь стоит подумать:
-        // какие именно перемещения нам необходимы для переноса пирамидки из двух дисков?
+        // определим, какой стержень остался для промежуточных действий,
+        // учитывая указанные исходный и целевой стержни
+        intermediate_rod = get_intermediate_rod(departure_rod, destination_rod)
+
+        // перекладываем маленький диск на промежуточный стержень
+        move(departure_rod, intermediate_rod)
+        
+        // перекладываем большой диск на целевой стержень
+        move(departure_rod, destination_rod)
+        
+        // маленький диск с промежуточного стержня перекладываем на целевой
+        move(intermediate_rod, destination_rod)
     }
 }
